936b3f917e705831ffef21db486f52f911a62094
maisyadhia <maisyaazzahra05@gmail.com>
2025-10-18 13:55:33 +0700

    add column and row to body

--- lib/main.dart
+++ lib/main.dart
@@ -15,24 +15,20 @@ class Sib3a extends StatelessWidget {
           title: Text('Flutternya Maisya'),
           backgroundColor: Colors.purple,
         ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(color: Colors.blue),
-                child: Text(
-                  'Drawer Header',
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-              ),
-              ListTile(leading: Icon(Icons.message), title: Text('Messages')),
-              ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Text('Profile')),
-              ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
-            ],
-          ),
+        drawer: Drawer(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You have ...'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star, color: Colors.amber),
+                Icon(Icons.star, color: Colors.amber),
+                Icon(Icons.star_half, color: Colors.amber),
+              ],
+            ),
+          ],
         ),
       ),
     );
